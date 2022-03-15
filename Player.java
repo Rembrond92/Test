4502eeb2eceef7633a7bc871f5d86b9b1927050b
Rembrond92 <99729164+Rembrond92@users.noreply.github.com>
2022-03-15 22:36:18 +0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,14 @@
+class Player {
+
+    protected String name;
+    protected int victories;
+    protected int defeats;
+    protected int draws;
+
+    protected Player(String name, int v, int def, int dr) {
+        this.name = name;
+        this.victories = v;
+        this.defeats = def;
+        this.draws = dr;
+    }
+}
